ab5d7465f67c04d36ea53e8ddf070f6e891dfe5c
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-15 10:32:07 +0600

    server connet done  with mongoDB

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors')
+const req = require('express/lib/request')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 require('dotenv').config()
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 5000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+// soloSphere
+// dtVWNAJocWFGuwsx
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.wr4sb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,21 +23,21 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 })
-
+app.get('/', (req, res) => {
+  res.send('Hello Runng SoloSphere Server....')
+})
 async function run() {
   try {
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    // await client.db('admin').command({ ping: 1 })
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
+      'Hey Juabyer You successfully connected to MongoDB!'
     )
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
 run().catch(console.dir)
-app.get('/', (req, res) => {
-  res.send('Hello from SoloSphere Server....')
-})
-
 app.listen(port, () => console.log(`Server running on port ${port}`))
